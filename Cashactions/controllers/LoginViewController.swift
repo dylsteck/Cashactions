4601168afad90384dda6cbef08e821476f0b461b
Dylan Steck <dylancsteck@gmail.com>
2018-04-10 22:20:56 -0400

    sign in fully works, now need db for transactions

--- Cashactions/controllers/LoginViewController.swift
+++ Cashactions/controllers/LoginViewController.swift
@@ -12,7 +12,8 @@ import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,12 +26,40 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func signUp(_ sender: Any) {
-        Auth.auth().createUser(withEmail: usernameTextField.text!, password: passwordTextField.text!) { (user, error) in
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if let error = error { // if there's an error
+                print(error.localizedDescription)
+            }
+                
+            else if let user = user{ // if there's no error
+                print("no error in sign up")
+                print(user.email)
+                
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = self.nameTextField.text!
+                changeRequest.commitChanges(completion: { error in // adding display name to user
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                    else{
+                        print("no error in adding display name")
+                        print(user.displayName)
+                    }
+                })
+            }
         }
     }
     
     @IBAction func logIn(_ sender: Any) {
-        Auth.auth().signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!) { (user, error) in
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if let error = error { // if there's an error
+                print(error.localizedDescription)
+            }
+                
+            else if let user = user{ // if there's no error
+                print("no error in log in")
+                print(user.email)
+            }
         }
     }
 
