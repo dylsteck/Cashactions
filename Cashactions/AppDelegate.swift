93b97c98cbd552a0557d04ceffd918c30a93a231
Dylan Steck <dylancsteck@gmail.com>
2018-05-14 07:35:19 -0400

    removed error

--- Cashactions/AppDelegate.swift
+++ Cashactions/AppDelegate.swift
@@ -28,10 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-              try! Auth.auth().signOut()
-         //   if Auth.auth().currentUser != nil {
-           //     self.segueStoryboard()
-            //}
+         //     try! Auth.auth().signOut()
+            if Auth.auth().currentUser != nil {
+                self.segueStoryboard()
+            }
         return true
     }
 
